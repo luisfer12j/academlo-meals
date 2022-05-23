0bf9f7ec36301c99e7d4e4edad222ab6c3c82452
Luis Sanchez <luisfer1299sj@gmail.com>
2022-05-23 08:39:51 -0500

    fixed db configuration

--- utils/database.js
+++ utils/database.js
@@ -10,6 +10,12 @@ const db = new Sequelize({
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
     logging: false,
+    dialectOptions: process.env.NODE_ENV === 'production' ? {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    } : {}
 })
 
 module.exports = { db };
